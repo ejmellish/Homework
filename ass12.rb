39f009470a307f821aef043ff3cf92cbd56bbff5
Ethan Mellish <mellishethan@yahoo.com>
2015-01-29 14:54:18 -0500

    assignment 12 incoming

---
+++ ass12.rb
@@ -0,0 +1,10 @@
+#question
+q_array = [" What is your favorite food?", " What is your favorite NFL team?"]
+
+#loop with each
+q_array.each do |question|
+  print question
+  u_input = gets.chomp
+
+
+end
